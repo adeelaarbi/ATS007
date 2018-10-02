5c020ad46165cc8cefc5936ac1eb0fca3f95b217
adeel <adeel.aarbi92@gmail.com>
2018-10-02 20:58:45 +0500

    .

---
+++ ATS007/Controllers/AtsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ATS007.Controllers
+{
+    public class AtsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("calendar")]
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+    }
+}
